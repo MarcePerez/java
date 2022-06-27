5f78cfe88a4659df5ab533e73b64fbc8ccecf4de
unknown <mdpymeg@gmail.com>
2022-06-26 23:59:37 -0300

    Ejercicios del nivel 3

---
+++ EjerciciosComplementariosLevel3/Ejercicio2.java
@@ -0,0 +1,16 @@
+/*import java.util.List;
+import java.util.stream.Collectors;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        List<Integer> palabras = List.of(1, 2, 3, 4, 5);
+
+        List<Integer> potenciaDeNumerosAlCuadrado = palabras.stream()
+            .map(x -> x * x)
+            .collect(Collectors.toList());
+
+            System.out.println(potenciaDeNumerosAlCuadrado);
+    
+    }
+}
+*/
---
+++ EjerciciosComplementariosLevel3/Ejercicio3.java
@@ -0,0 +1,18 @@
+/*import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        List<String> nombres = List.of("Batman", "Aquaman", "Flash", "Batgirl", "Wonder Woman", "brainiac");
+
+        long filtro = nombres.stream()
+            .valueOf(nombres.charAt(0)
+            .count();
+        System.out.println(filtro);
+
+
+    }
+}
+*/
